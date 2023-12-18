eff322888e3893ef4b44f2f81e4496f9bdbbc80f
XXXUDI <sashamlgg@gmail.com>
2023-12-18 04:50:18 +0200

    init comit

---
+++ src/test/java/com/soCompany/SpringBootTutorial/CompanyServiceIT.java
@@ -0,0 +1,24 @@
+package com.soCompany.SpringBootTutorial;
+
+import com.soCompany.ApplicationRunner;
+import com.soCompany.service.CompanyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = ApplicationRunner.class)
+@ActiveProfiles("test")
+public class CompanyServiceIT {
+
+    private static final Integer COMPANY_ID = 1;
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+
+    }
+
+}
