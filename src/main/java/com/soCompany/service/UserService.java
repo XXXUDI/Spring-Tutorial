fea68932ee7ec16af37288a06736e6054ddf8073
XXXUDI <sashamlgg@gmail.com>
2023-12-29 17:59:26 +0200

    Crud application -> UserService

--- src/main/java/com/soCompany/service/UserService.java
+++ src/main/java/com/soCompany/service/UserService.java
@@ -1,19 +1,60 @@
 package com.soCompany.service;
 
+import com.soCompany.database.EmployeeRepository;
 import com.soCompany.database.UserRepository;
 import com.soCompany.dto.UserEditCreateDto;
+import com.soCompany.dto.UserReadDto;
+import com.soCompany.entity.Employee;
 import com.soCompany.mapper.UserEditCreateMapper;
 import com.soCompany.mapper.UserReadMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
-    private final UserRepository userRepository;
+    private final EmployeeRepository repository;
     private final UserReadMapper userReadMapper;
     private final UserEditCreateMapper userEditCreateMapper;
 
+    public List<UserReadDto> findAll() {
+        return repository.findAll().stream().map(userReadMapper::map).toList();
+    }
 
+    public Optional<UserReadDto> findById(Integer id) {
+        return repository.findById(id).map(userReadMapper::map);
+    }
+
+    @Transactional
+    public UserReadDto create(UserEditCreateDto readDto) {
+        return Optional.of(readDto)
+                .map(userEditCreateMapper::map)
+                .map(repository::save)
+                .map(userReadMapper::map)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public Optional<UserReadDto> update(Integer id, UserEditCreateDto userDto) {
+        return repository.findById(id)
+                .map(employee -> userEditCreateMapper.map(userDto, employee))
+                .map(repository::saveAndFlush)
+                .map(userReadMapper::map);
+    }
+
+    @Transactional
+    public boolean delete(Integer id) {
+        return repository.findById(id)
+                .map(employee -> {
+                    repository.delete(employee);
+                    repository.flush();
+                    return true;
+                }).orElse(false);
+    }
 }
